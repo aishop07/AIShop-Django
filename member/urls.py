ec13eda22f8b03f949c37d0529c788fc037ed813
鄭又華 <ln198301111@yahoo.com>
2018-07-19 13:10:18 +0800

    urls.py format

--- member/urls.py
+++ member/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'member'
 
 urlpatterns = [
-    path('',views.login,name='login')
-
+    path('',views.index,name='index'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('forget/',views.forget,name='forget'),
+    path('create',views.create,name='create')
 ]
